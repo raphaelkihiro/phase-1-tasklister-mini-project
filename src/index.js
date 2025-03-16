9585485e2fa73b43b34658d585ab000ab1005b53
raphaelkihiro <kihirongigi@gmail.com>
2025-03-16 09:47:57 +0300

    code-challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+    document.querySelector("form").addEventListener("submit", (event) => {
+        event.preventDefault();
+        const newTask = document.getElementById("new-task-description").value;
+        console.log(newTask);
+    });
+    const li = document.createElement("li");
+    li.textContent = task;
+    li.appendChild(button);
+    const theList = document.querySelector("#tasks");
+    theList.appendChild(li);
 });
+
